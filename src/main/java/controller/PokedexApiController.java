3f1a1969f83d1cfe085819fdb82894f7b90deb87
Luiz Henrique Figueira <luiz.h.figueira@accenture.com>
2018-10-04 23:40:35 -0300

    Mock Json.

--- src/main/java/controller/PokedexApiController.java
+++ src/main/java/controller/PokedexApiController.java
@@ -3,10 +3,18 @@ package controller;
 import me.sargunvohra.lib.pokekotlin.client.PokeApi;
 import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
 import me.sargunvohra.lib.pokekotlin.model.NamedApiResource;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 @RequestMapping("/api")
@@ -20,4 +28,22 @@ public class PokedexApiController {
 
         return list;
     }
+
+    @CrossOrigin
+    @GetMapping("/getAllMock")
+    public JSONArray getAllMock() throws IOException, ParseException {
+       String path = "src/resources/mock.json";
+
+        // parsing file "JSONExample.json"
+        Object obj = new JSONParser().parse(new FileReader(path));
+
+        // typecasting obj to JSONObject
+        JSONArray json = (JSONArray) obj;
+
+
+
+        return json;
+    }
+
+
 }
