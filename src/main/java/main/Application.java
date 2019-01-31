32262fd1ba9632225730e941d99f57d7a9d60bbe
Luiz Henrique Figueira <luiz.h.figueira@accenture.com>
2019-01-31 18:14:00 -0200

    Dependency Update and Cors Configuration.

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -3,13 +3,53 @@ package main;
 import controller.PokedexApiController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = PokedexApiController.class)
 public class Application {
 
+    private String allowedOrigin = "localhost:8080";
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(Application.this.allowedOrigin)
+                        .allowedMethods("POST", "GET", "DELETE", "PUT", "OPTIONS", "PATCH")
+                        .allowedHeaders("Authorization", "Content-Type", "Accept")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 }
